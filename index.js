957ddb40d5074a403f97c8654df7bd81b808608c
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2017-05-29 08:35:20 +0200

    chore(*): Reduce the size of the package
    Make the package ES5-compatible, so transpilation is not needed.

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
 
-const msgpack = require('notepack.io');
-const Emitter = require('component-emitter');
+var msgpack = require('notepack.io');
+var Emitter = require('component-emitter');
 
 /**
  * Packet types (see https://github.com/socketio/socket.io-protocol)
  */
 
-const TYPES = {
-  CONNECT: 0,
-  DISCONNECT: 1,
-  EVENT: 2,
-  ACK: 3,
-  ERROR: 4,
-  BINARY_EVENT: 5,
-  BINARY_ACK: 6
-};
+exports.CONNECT = 0;
+exports.DISCONNECT = 1;
+exports.EVENT = 2;
+exports.ACK = 3;
+exports.ERROR = 4;
+exports.BINARY_EVENT = 5;
+exports.BINARY_ACK = 6;
 
-const errorPacket = {
-  type: TYPES.ERROR,
+var errorPacket = {
+  type: exports.ERROR,
   data: 'parser error'
 };
 
-class Encoder {
-  encode (packet, callback) {
-    switch (packet.type) {
-      case TYPES.CONNECT:
-      case TYPES.DISCONNECT:
-      case TYPES.ERROR:
-        return callback([ JSON.stringify(packet) ]);
-      default:
-        return callback([ msgpack.encode(packet) ]);
-    }
-  }
-}
+function Encoder () {}
 
-class Decoder extends Emitter {
-  add (obj) {
-    if (typeof obj === 'string') {
-      this.parseJSON(obj);
-    } else {
-      this.parseBinary(obj);
-    }
+Encoder.prototype.encode = function (packet, callback) {
+  switch (packet.type) {
+    case exports.CONNECT:
+    case exports.DISCONNECT:
+    case exports.ERROR:
+      return callback([ JSON.stringify(packet) ]);
+    default:
+      return callback([ msgpack.encode(packet) ]);
   }
-  parseJSON (obj) {
-    try {
-      let decoded = JSON.parse(obj);
-      this.emit('decoded', decoded);
-    } catch (e) {
-      this.emit('decoded', errorPacket);
-    }
-  }
-  parseBinary (obj) {
-    try {
-      let decoded = msgpack.decode(obj);
-      this.emit('decoded', decoded);
-    } catch (e) {
-      this.emit('decoded', errorPacket);
-    }
-  }
-  destroy () {}
-}
+};
 
-module.exports = { Encoder, Decoder };
+function Decoder () {}
+
+Emitter(Decoder.prototype);
+
+Decoder.prototype.add = function (obj) {
+  if (typeof obj === 'string') {
+    this.parseJSON(obj);
+  } else {
+    this.parseBinary(obj);
+  }
+};
+
+Decoder.prototype.parseJSON = function (obj) {
+  try {
+    var decoded = JSON.parse(obj);
+    this.emit('decoded', decoded);
+  } catch (e) {
+    this.emit('decoded', errorPacket);
+  }
+};
+
+Decoder.prototype.parseBinary = function (obj) {
+  try {
+    var decoded = msgpack.decode(obj);
+    this.emit('decoded', decoded);
+  } catch (e) {
+    this.emit('decoded', errorPacket);
+  }
+};
+
+Decoder.prototype.destroy = function () {};
+
+exports.Encoder = Encoder;
+exports.Decoder = Decoder;
